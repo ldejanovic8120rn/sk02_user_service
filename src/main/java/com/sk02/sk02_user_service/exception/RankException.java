a95ab108094e74752803b0071c67eb367871800d
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2022-01-13 18:05:21 +0100

    GMTA - sk02_user_service

--- src/main/java/com/sk02/sk02_user_service/exception/RankException.java
+++ src/main/java/com/sk02/sk02_user_service/exception/RankException.java
@@ -1,7 +1,9 @@
 package com.sk02.sk02_user_service.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NO_CONTENT)
 public class RankException extends HttpResponseException{
 
     public RankException(String message) {
